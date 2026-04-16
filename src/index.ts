af721efdae0d09317219cdeaf0e4a52dec0a02da
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-16 08:49:39 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ export const isValidStacksAddress = (address: string): boolean => {
   }
 };
 /**
- * Identifies the network type of a Stacks address.
+ * Identifies the network type of  Stacks address.
  */
 export const getStacksNetwork = (address: string): 'mainnet' | 'testnet' | 'unknown' => {
   if (!isValidStacksAddress(address)) return 'unknown';
