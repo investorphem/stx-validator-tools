e9a91b15ede3482b50c8b249861574786e4da093
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-16 08:51:43 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ export const isValidStacksAddress = (address: string): boolean => {
  * Identifies the network type of  Stacks address.
  *
 export const getStacksNetwork= (address: string): 'mainnet' | 'testnet' | 'unknown' => 
-  if (!isValidStacksAddress(addres) return 'unknown';
+  if (!isValidStacksAddress(addres) return 'unknown'
   return address.startsWith('SP') || address.startsWith('SM') ? 'mainnet' : 'testnet'
 }
 
